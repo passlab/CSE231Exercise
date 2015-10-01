bb6d752285109b8009981010ec3a024eb19d2c67
Yonghong Yan <yanyh15@gmail.com>
2015-10-01 09:55:34 -0400

    source code for chapter 05

--- src/ch04/BlobApp.java
+++ src/ch04/BlobApp.java
@@ -8,33 +8,44 @@ package ch04;
 
 import java.util.Scanner;
 
-public class BlobApp
-{
-  public static void main(String[] args)
-  {
-    Scanner conIn = new Scanner(System.in);
+public class BlobApp {
+    public static void main(String[] args) {
+        Scanner conIn = new Scanner(System.in);
 
-    final int GRIDR = 10;   // number of grid rows
-    final int GRIDC = 40;   // number of grid columns
+        final int GRIDR = 10;   // number of grid rows
+        final int GRIDC = 40;   // number of grid columns
 
-    // Get percentage probability of blob characters
-    int percentage;       
-    System.out.print("Input percentage probability (0 to 100): ");
-    if (conIn.hasNextInt())
-      percentage = conIn.nextInt();
-    else
-    {
-      System.out.println("Error: you must enter an integer.");
-      System.out.println("Terminating program.");
-      return;
+        // Get percentage probability of blob characters
+        int percentage;
+        System.out.print("Input percentage probability (0 to 100): ");
+        if (conIn.hasNextInt())
+            percentage = conIn.nextInt();
+        else {
+            System.out.println("Error: you must enter an integer.");
+            System.out.println("Terminating program.");
+            return;
+        }
+        System.out.println();
+
+        // create grid
+        Grid grid = new Grid(GRIDR, GRIDC, percentage);
+
+        // display grid and blob count
+        System.out.println(grid);
+        System.out.println("\nThere are " + grid.blobCount() + " blobs.\n");
+
+        int i;
+        int numBlobs = 0;
+        int maxBlobsPercentage = 0;
+        for (i = 0; i < 100; i++) {
+            grid = new Grid(GRIDR, GRIDC, i);
+            int temp = grid.blobCount();
+            if (numBlobs < temp) {
+                numBlobs = temp;
+                maxBlobsPercentage = i;
+            }
+        }
+        System.out.println("\nmax # blobs " + numBlobs + " with " + maxBlobsPercentage + "%.\n");
+
     }
-    System.out.println();
-
-    // create grid
-    Grid grid = new Grid(GRIDR, GRIDC, percentage);
-    
-    // display grid and blob count
-    System.out.println(grid);
-    System.out.println("\nThere are " + grid.blobCount() + " blobs.\n");
-  }
 }
